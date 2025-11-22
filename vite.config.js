145f2aa6e2c11914367388b8e6869d5a09b132db
yogesh4uamu <yogesh4uamu@gmail.com>
2025-11-22 21:14:22 +0530

    Updating config.

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-    allowedHosts: ["g8p8cp-3000.csb.app"],
+    allowedHosts: ["https://three60-fit-hub-satara.onrender.com/"],
   },
   resolve: {
     alias: {
